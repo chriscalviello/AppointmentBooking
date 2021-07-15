6121d89414304140ecd560e2f69a635810d50ac2
Chris Calviello <chris.calviello@gmail.com>
2021-07-15 16:13:56 +0200

    add deps

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,16 +1,20 @@
 import { ConcreteAuthenticationService } from "./services/authentication/concrete";
+import { ConcreteBookingService } from "./services/booking/concrete";
 import { ConcreteUserService } from "./services/user/concrete";
 import JwtConfig from "./config/jwt";
 import App from "./app";
+
+const userService = new ConcreteUserService();
 
 const app = new App(
   5000,
   new ConcreteAuthenticationService(
     JwtConfig.accessTokenSecret,
     JwtConfig.refreshTokenSecret,
-    20
+    60 * 10
   ),
-  new ConcreteUserService()
+  new ConcreteBookingService(userService),
+  userService
 );
 
 app.start();
